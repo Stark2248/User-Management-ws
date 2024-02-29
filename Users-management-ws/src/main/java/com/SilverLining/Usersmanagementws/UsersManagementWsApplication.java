8d7d61c8a49d372a4883dfb2ff786be9fc04c126
Amlan <dasamlan71@gmail.com>
2024-02-29 23:45:58 +0530

    commiting default code

---
+++ Users-management-ws/src/main/java/com/SilverLining/Usersmanagementws/UsersManagementWsApplication.java
@@ -0,0 +1,13 @@
+package com.SilverLining.Usersmanagementws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagementWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagementWsApplication.class, args);
+	}
+
+}
---
+++ Users-management-ws/src/test/java/com/SilverLining/Usersmanagementws/UsersManagementWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SilverLining.Usersmanagementws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
